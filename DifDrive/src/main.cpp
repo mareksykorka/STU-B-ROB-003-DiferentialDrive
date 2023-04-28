461551d0bf8668f7fd45e4be1491c03487829c47
Marek Sýkorka <marcopolo1234565@gmail.com>
2023-04-29 00:54:15 +0200

    DifDrive v0.2.0  + semi-working model v2

--- DifDrive/src/main.cpp
+++ DifDrive/src/main.cpp
@@ -6,8 +6,10 @@
 #define DEFAULT_WHEEL_RADIUS (float)0.05
 #define DEFAULT_WHEELBASE ((float)0.1)*2
 
+#define TIME_MILI ((float)0.001)
+
 class Wheel {
-	public:
+	private:
 		float wheelRadius;
 		float angularVel;
 		float tangentialVel;
@@ -25,15 +27,13 @@ class Wheel {
 		}
 
 		float getTangentialVel() {
+			tangentialVel = this->angularVel * wheelRadius;
 			return this->tangentialVel;
 		}
 };
 
 class Vehicle {
 	public:
-		Wheel lWheel = Wheel(DEFAULT_WHEEL_RADIUS);
-		Wheel rWheel = Wheel(DEFAULT_WHEEL_RADIUS);
-
 		float wheelbase;
 		float angularVel;
 		float tangentialVel;
@@ -43,6 +43,9 @@ class Vehicle {
 		float y;
 
 	public:
+		Wheel lWheel = Wheel(DEFAULT_WHEEL_RADIUS);
+		Wheel rWheel = Wheel(DEFAULT_WHEEL_RADIUS);
+
 		Vehicle(float _wheelbase) {
 			this->wheelbase = _wheelbase;
 			this->angularVel = 0;
@@ -71,65 +74,126 @@ class Vehicle {
 			double deltaX = vX * deltaTime;
 			double deltaY = vY * deltaTime;
 
-			this->x = this->x + deltaX;
-			this->y = this->y + deltaY;
+			this->x += deltaX;
+			this->y += deltaY;
 		}
 
-		void printData(double time) {
-			printf("%5.2f %2.2f %2.2f\n", time, this->x, this->y);
+		void printData(double timeDelta, double time) {
+			printf("%f | %3.4f | %f | %f \n", timeDelta, time, this->x, this->y);
 		}
 };
 
+int main() {
+	//Declare and create a new render-window
+	sf::RenderWindow window(sf::VideoMode(1280,720), "SFML window");
+	window.display();
 
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML Line");
+	// Event processing
+	sf::Event event;
 
+	// Count timer
+	auto absolute_start_time = std::chrono::high_resolution_clock::now(); // get current time
+	auto start_time = std::chrono::high_resolution_clock::now(); // get current time
+
+	// Model logic
+	Vehicle vehicle(DEFAULT_WHEELBASE);
+	vehicle.lWheel.setAngularVel(200);
+	vehicle.rWheel.setAngularVel(200);
+
+	double i = 0;
+
+	// The main loop - ends as soon as the window is closed
 	while (window.isOpen())
 	{
-		sf::Event event;
+		auto end_time = std::chrono::high_resolution_clock::now(); // get current time again
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time); // calculate time difference
+		double absolute_duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - absolute_start_time).count() * TIME_MILI; // calculate time difference
+
+		// Handle Events
 		while (window.pollEvent(event))
 		{
+			// Request for closing the window
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (event.type == sf::Event::KeyPressed) {
+				// Handle key press event
+
+				switch (event.key.code)
+				{
+					case sf::Keyboard::Key::W:
+						printf("W");
+						break;
+					case sf::Keyboard::Key::A:
+						printf("A");
+						break;
+					case sf::Keyboard::Key::S:
+						printf("S");
+						break;
+					case sf::Keyboard::Key::D:
+						printf("D");
+						break;
+					case sf::Keyboard::Key::Q:
+						printf("Q");
+						break;
+					case sf::Keyboard::Key::E:
+						printf("E");
+						break;
+					default:
+						break;
+				}
+			}
 		}
-		
-		Vehicle vehicle(DEFAULT_WHEELBASE);
-		vehicle.lWheel.setAngularVel(1);
 
-		double prevTime = 0, time = prevTime + 1;
-		while (true) {
-			sf::VertexArray line(sf::LineStrip, 2);
-			line[0].position = sf::Vector2f((vehicle.x*500) + 300, (vehicle.y * 500) + 200);
-			line[0].color = sf::Color::Red;
+		if (absolute_duration >= 0) {
+			vehicle.lWheel.setAngularVel(2);
+		 	vehicle.rWheel.setAngularVel(2);
+		}
+		if (absolute_duration >= 5) {
+			vehicle.lWheel.setAngularVel(-1);
+			vehicle.rWheel.setAngularVel(1);
+		}
+		if (absolute_duration >= 10) {
+			vehicle.lWheel.setAngularVel(0);
+			vehicle.rWheel.setAngularVel(0);
+		}
+		if (absolute_duration >= 15) {
+			vehicle.lWheel.setAngularVel(2);
+			vehicle.rWheel.setAngularVel(-2);
+		}
+		if (absolute_duration >= 20) {
+			vehicle.lWheel.setAngularVel(1);
+			vehicle.rWheel.setAngularVel(1);
+		}
+		if (absolute_duration >= 25) {
+			vehicle.lWheel.setAngularVel(2);
+			vehicle.rWheel.setAngularVel(0);
+		}
+		if (absolute_duration >= 35) {
+			vehicle.lWheel.setAngularVel(0);
+			vehicle.rWheel.setAngularVel(-2);
+		}
+		if (absolute_duration >= 50) {
+			vehicle.lWheel.setAngularVel(10000);
+			vehicle.rWheel.setAngularVel(0);
+		}
 
-			vehicle.calculateXY(time - prevTime);
-			vehicle.printData(time);
+		// Calculate data
+		if (duration.count() >= 10) { // check if 10ms has elapsed
+			vehicle.calculateXY(duration.count() * TIME_MILI);
+			vehicle.printData(duration.count() * TIME_MILI, absolute_duration);
+			start_time = std::chrono::high_resolution_clock::now(); // reset start time
+
+			// Draw some graphical entities
+			sf::CircleShape point(4.f);
+			point.setPosition(((vehicle.x) * 500) + 600, ((vehicle.y) * 500) + 300);
+			point.setFillColor(sf::Color::Red);
 			
-			line[1].position = sf::Vector2f((vehicle.x * 500) + 300, (vehicle.y * 500) + 200);
-			line[1].color = sf::Color::Red;
-
-			window.draw(line);
-			window.display();
-
-			prevTime = time;
-			time += 0.005;
-			//std::this_thread::sleep_for(std::chrono::milliseconds(0));
-
-			if (time > 30)
-				vehicle.rWheel.setAngularVel(2);
-
-			if (time > 40)
-				vehicle.lWheel.setAngularVel(4);
-
-
-			if (time > 5000)
-				break;
+			//window.clear();
+			window.draw(point);
+			//window.display();
 		}
-		window.clear();
 		window.display();
-		break;
 	}
-
 	return 0;
 }
